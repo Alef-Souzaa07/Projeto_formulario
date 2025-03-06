670292da0353d31d4d94d9a7d3e36b1f36236de1
Alef <alef.programer@gmail.com>
2025-03-06 08:42:39 -0300

    Criaçao da lista de usuarios - conexao co banco de dados mongoDB - Utilização do prisma para interagir co o banco de dados - criaçao do back end

---
+++ server.js
@@ -0,0 +1,47 @@
+import express from 'express'; //Tem que adicionar o json no express
+import { PrismaClient } from '@prisma/client'
+
+const prisma = new PrismaClient()
+
+const app = express();
+app.use(express.json());
+
+app.post('/usuarios', async (req, res) => {
+
+    await prisma.user.create({
+        data: {
+            email:   req.body.email,
+            name:    req.body.name,
+            sex:    req.body.sex,
+            age:    req.body.age  
+        }
+    });
+
+    res.status(201).json(req.body);
+
+});
+
+app.get('/usuarios', async (request, response) => {
+    
+    const users = await prisma.user.findMany();
+
+    response.status(200).json(users);
+    
+});
+/*  Precisa do tipo de rota / metodo HTTP 
+    Endereço
+    */
+   app.listen(3000);
+
+/*Criar nossa API  de usuarios
+-Criar um usuario    
+-listar todos os usuarios
+-Editar um usuario
+-Deletar um usuario*/
+
+/*Tipo de rota / metodo HTTP 
+  Endereço
+  
+  alefranio07
+  Alef2001
+  */
